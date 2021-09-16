fed39169d3cde40f7413c31aaf3b8cb70afb4666
Lenovo <Lenovo@LAPTOP-4N2U9Q2V>
2021-09-16 22:13:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Arraydemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the rows for star pattern");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 1; j <=i; j++)
+                {
+                    Console.Write("*\t");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
